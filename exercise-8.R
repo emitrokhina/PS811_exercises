790362b1940c282fbf19f22f04cd2f1ed4e57c81
emitrokhina <emmitrokhina@gmail.com>
2020-11-15 16:10:26 +0300

    exercise 8 (not all)

---
+++ exercise-8.R
@@ -0,0 +1,54 @@
+####Exercise 8 for PS811
+###Evgeniya Mitrokhia
+#15.11.2020
+
+library(tidyverse)
+library(ggplot2)
+
+#1
+USArrests <- USArrests
+
+#2
+plot(USArrests$Murder, USArrests$Assault, main = "Correlation between murder \n and assault arrests", 
+     xlab = "Murder",
+     ylab = "Assault")
+abline(lm(Assault ~ Murder, data = USArrests), col="red")
+
+
+ggplot(USArrests, aes(Murder, Assault)) + 
+  labs(title = "Correlation between murder \n and assault arrests", y = "Assault",  x = "Murder") +
+  geom_smooth(method = "lm") +
+  geom_point()
+ 
+#3
+boxplot(USArrests$Rape,
+        main = "Rape arrests",
+        ylab = "Number of rape arrests")
+
+ggplot(USArrests, aes(Rape)) + 
+  labs(title = "Rape arrests",  x = "Number of rape arrests") +
+  geom_boxplot()
+
+#4
+USArrests[ "State" ] <- rownames(USArrests)
+
+barplot(USArrests$Rape, main = "Number of rape arrests per state",
+        xlab = "State",
+        ylab = "Number of rape arrests",
+        names.arg = c(USArrests$State),
+        las=2)
+
+ggplot(USArrests, aes(factor(State), Rape)) + 
+  geom_bar(stat = 'identity') +
+  labs(title = "Number of rape arrests per state", y = "Number of rape arrests",  x = "State") +
+  theme(axis.text.x = element_text(angle=90, hjust=1))
+
+#5
+hist(USArrests$UrbanPop, main = "Urban Population crime frequency", 
+     xlab = "Percent of urban population")
+
+ggplot(USArrests, aes(UrbanPop)) + 
+  labs(title = "Urban Population crime frequency", y = "Frequency",  x = "Urban Population") +
+  geom_histogram(binwidth = 5, color="black", fill="white")
+
+
